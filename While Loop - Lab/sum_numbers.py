36f58f30f4a40a2e72bf44fa0f8e08f178778b14
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-08-17 19:00:56 +0300

    Solutions to tasks from While Loop - Lab
    Solutions to tasks from Python-Basics While Loop - Lab
    https://judge.softuni.org/Contests/Compete/DownloadResource/17789

---
+++ While Loop - Lab/sum_numbers.py
@@ -0,0 +1,9 @@
+first_number = int(input())
+
+sum_all_numbers = 0
+
+while sum_all_numbers < first_number:
+    curr_number = int(input())
+    sum_all_numbers += curr_number
+
+print(sum_all_numbers)
