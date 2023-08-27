fa94eab9b49d482b4a22c9f8125b81470a2faabb
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-08-27 20:54:08 +0300

    Solutions to tasks from Nested Loops - More Exercises

---
+++ Nested Loops - More Exercises/secret_door's_lock.py
@@ -0,0 +1,10 @@
+upper_limit_1 = int(input())
+upper_limit_2 = int(input())
+upper_limit_3 = int(input())
+
+for digit_1 in range(2, upper_limit_1 + 1, 2):
+    for digit_2 in range(2, upper_limit_2 + 1):
+        if digit_2 == 2 or digit_2 == 3 or digit_2 == 5 or digit_2 == 7:
+            for digit_3 in range(2, upper_limit_3 + 1, 2):
+                print(f"{digit_1} {digit_2} {digit_3}")
+              
