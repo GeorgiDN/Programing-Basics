469151c80ffb2a3a7fbaac2c8bfba0756c93ec21
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-14 20:07:54 +0200

    Update christamаss_tree.py

--- Drawing Figures with Loops - More Exercises/christamаss_tree.py
+++ Drawing Figures with Loops - More Exercises/christamаss_tree.py
@@ -1,7 +1,13 @@
 n = int(input())
 
-for row in range(n+1):
-    spaces = " " * (n - row)
-    stars = "*" * row + ' '
-    space_1 = ' '
-    print(f"{spaces}{stars}|{space_1}{stars}")
+for i in range(n + 1):
+    print(" " * (n - i) + ("*" * i) + " | " + ("*" * i))
+
+
+# n = int(input())
+
+# for row in range(n+1):
+#     spaces = " " * (n - row)
+#     stars = "*" * row + ' '
+#     space_1 = ' '
+#     print(f"{spaces}{stars}|{space_1}{stars}")
