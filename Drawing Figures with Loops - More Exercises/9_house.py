3019698484e9d167f1e969d014e06d3c9a92d102
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-01-14 20:08:48 +0200

    Create 9_house.py

---
+++ Drawing Figures with Loops - More Exercises/9_house.py
@@ -0,0 +1,18 @@
+roof_rows = (n + 1) // 2
+
+for row in range(roof_rows):
+    if row == 0:
+        if n % 2 == 0:
+            print("-" * ((n // 2) - 1) + "**" + "-" * ((n // 2) - 1))
+        elif n % 2 != 0:
+            print("-" * int(((n + 1) // 2) - 1) + "*" + "-" * int(((n + 1) // 2) - 1))
+    else:
+        if n % 2 == 0:
+            print("-" * (((n // 2) - 1) - row) + "*" + "*" * (2 * row + 1) + "-" * (((n // 2) - 1) - row))
+        elif n % 2 != 0:
+            print("-" * (((n + 1) // 2) - row - 1) + "*" + "*" * (2 * row) + "-" * (((n + 1) // 2) - row - 1))
+
+
+base_rows = (n // 2) - 1
+for base_row in range(base_rows + 1):
+    print("|" + "*" * (n - 2) + "|")
