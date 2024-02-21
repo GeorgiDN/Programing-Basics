b735470149c6ebc097200f502705a97a71bb97e5
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-21 22:20:31 +0200

    Update numbers_from_1_to_100.py

--- For Loop - Lab/numbers_from_1_to_100.py
+++ For Loop - Lab/numbers_from_1_to_100.py
@@ -1,3 +1,17 @@
+def recursion_numbers(n):
+    if n > 100:
+        return n
+    else:
+        print(n)
+        return recursion_numbers(n + 1)
+
+
+num = 1
+recursion_numbers(num)
+
+
+
+# Basic
 for number in range(1, 101):
     print(number)
     
