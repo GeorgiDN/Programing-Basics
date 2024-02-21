4671ff6176d319bca8e7daaf70b80da73f76777f
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-21 22:21:08 +0200

    Update numbers_1_n_with_step_3.py

--- For Loop - Lab/numbers_1_n_with_step_3.py
+++ For Loop - Lab/numbers_1_n_with_step_3.py
@@ -1,3 +1,18 @@
-n = int(input())
-for num in range(1, n + 1, 3):
-    print(num)
+def recursion_numbers(start_num_):
+    if start_num_ > final_num:
+        return
+    else:
+        print(start_num_)
+        recursion_numbers(start_num_ + 3)
+
+
+final_num = int(input())
+start_num = (final_num - final_num) + 1
+recursion_numbers(start_num)
+
+
+
+# Basic
+# n = int(input())
+# for num in range(1, n + 1, 3):
+#     print(num)
