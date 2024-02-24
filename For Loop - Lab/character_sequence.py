a74636451ca2d68c4aac355d58bccd9c2acb3f99
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-24 22:13:21 +0200

    Update character_sequence.py

--- For Loop - Lab/character_sequence.py
+++ For Loop - Lab/character_sequence.py
@@ -1,3 +1,15 @@
-word = input()
-for ch in word:
-    print(ch)
+def character_sequence(n=0):
+    if n == len(text):
+        return
+    else:
+        print(text[n])
+        return character_sequence(n+1)
+
+
+text = input()
+character_sequence()
+
+
+# word = input()
+# for ch in word:
+#     print(ch)
