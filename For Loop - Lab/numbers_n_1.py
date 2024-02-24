776b20cee5b3b5102aef6a44e30de86d736b0d0b
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-24 21:58:09 +0200

    Update numbers_n_1.py

--- For Loop - Lab/numbers_n_1.py
+++ For Loop - Lab/numbers_n_1.py
@@ -1,4 +1,18 @@
-number = int(input())
+# With recursion
+def numbers_in_reversed(n):
+    if n == 0:
+        return n
+    else:
+        print(n)
+        return numbers_in_reversed(n - 1)
 
-for num in range(number, 0, -1):
-    print(num)
+
+num = int(input())
+numbers_in_reversed(num)
+
+
+# Basic
+# number = int(input())
+
+# for num in range(number, 0, -1):
+#     print(num)
