b2828e7013648bc1a1d12932ae67eb60b2318e04
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-04-25 21:09:02 +0300

    Update numbers_ending_in_7.py

--- For Loop - Exercise/numbers_ending_in_7.py
+++ For Loop - Exercise/numbers_ending_in_7.py
@@ -1,2 +1,14 @@
-for num in range(7, 1000, 10):
-    print(num)
+def range_ten_from_seven(n=7):
+    if n + 10 > 1000:
+        print(n)
+        return
+    else:
+        print(n)
+        range_ten_from_seven(n + 10)
+
+
+range_ten_from_seven()
+
+
+# for num in range(7, 1000, 10):
+#     print(num)
