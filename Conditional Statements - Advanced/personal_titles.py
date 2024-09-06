11b14e9625ed5d5db10f68b937c15f0e2e2fdbcf
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-09-06 16:31:08 +0300

    added more solutions

--- Conditional Statements - Advanced/personal_titles.py
+++ Conditional Statements - Advanced/personal_titles.py
@@ -13,18 +13,42 @@ elif gender == 'f':
         print('Miss')
 
 
+##################################################################################
+age = float(input())
+gender = input()
 
-# Do not do this!
-        
-# age = float(input())
-# gender = input()
+title = 'Mr.' if gender == 'm' and age >= 16 else 'Master' \
+    if gender == 'm' else 'Ms.' if gender == 'f' and age >= 16 else 'Miss'
 
-# title = 'Mr.' if gender == 'm' and age >= 16 else 'Master' \
-#     if gender == 'm' else 'Ms.' if gender == 'f' and age >= 16 else 'Miss'
-
-# print(title)
+print(title)
 
 
+##################################################################################
+age, gender = float(input()),  input()
+print('Mr.' if gender == 'm' and age >= 16 else 'Master' if gender == 'm' else 'Ms.' if gender == 'f' and age >= 16 else 'Miss')
 
-# age, gender = float(input()),  input()
-# print('Mr.' if gender == 'm' and age >= 16 else 'Master' if gender == 'm' else 'Ms.' if gender == 'f' and age >= 16 else 'Miss')
+
+
+##################################################################################
+def personal_titles(*args):
+    age = float(args[0]),
+    gender = args[1]
+
+    dictionary = {
+        "m": [
+            {"min": 16, "title": "Mr."},
+            {"min": 0, "title": "Master"},
+        ],
+        "f": [
+            {"min": 16, "title": "Ms."},
+            {"min": 0, "title": "Miss"},
+        ]
+    }
+
+    for curren_age in dictionary[gender]:
+        if age >= curren_age["min"]:
+            return print(curren_age["title"])
+
+
+age, gender = float(input()), input()
+personal_titles(age, gender)
